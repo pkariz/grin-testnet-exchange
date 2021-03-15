8abec5283c4ac414c7227f792c71844eb311873b
vegycslol <vegycslol@gmail.com>
2021-03-15 22:58:48 +0100

    Fix: map ports only to localhost

--- docker/django/start-prod.sh
+++ docker/django/start-prod.sh
@@ -8,4 +8,4 @@ set -o xtrace
 pipenv run python manage.py migrate
 # for django-admin page
 pipenv run python manage.py collectstatic --noinput
-pipenv run gunicorn backend.wsgi --bind 0.0.0.0:8000
+pipenv run gunicorn backend.wsgi --bind 127.0.0.1:8000
--- docker/django/start.sh
+++ docker/django/start.sh
@@ -8,4 +8,4 @@ set -o xtrace
 pipenv run python manage.py migrate
 # for django-admin page
 pipenv run python manage.py collectstatic --noinput
-pipenv run python manage.py runserver 0.0.0.0:8000
+pipenv run python manage.py runserver 127.0.0.1:8000
